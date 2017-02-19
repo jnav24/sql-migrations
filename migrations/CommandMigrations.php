1f6c31708a320cde599759047049b6f6e13b7f27
Justin Navarro <justinn@liveanswer.com>
2017-02-18 20:48:48 -0500

    created a help command and a help guide

--- migrations/CommandMigrations.php
+++ migrations/CommandMigrations.php
@@ -4,42 +4,60 @@ namespace Migration;
 
 class CommandMigrations
 {
-	private $init_set = false;
-
-	public function __construct($migrate_obj)
+	public function __construct()
 	{
-		$this->migrate_obj = $migrate_obj;
+		$this->commands = $this->options();
 	}
 
 	public function options()
 	{
 		return [
-			'-h' => 'help',
-			'-i' => 'init',
-			'-e' => 'execute'
+			'-h' => 'display help commands',
+			'-i' => 'creates database and tables but does not import data',
+			'-e' => 'idk what this does',
+			'-m' => 'insert seeds in with the init migration'
 		];
 	}
 
-	public function initMigration()
+	public function listAllCommands()
 	{
-		if ($this->init_set) {
-			echo "Run '-i' before any other option\n";
-			return false;
+		echo "SQL Migrations\n";
+		echo "Author: Justin Navarro\n\n";
+		echo "Usage: migrate [options] [ -i ] [arguments]\n";
+		echo "\t migrate -i\n\n";
+		echo "Options:\n";
+
+		foreach ($this->commands as $option => $description) {
+			echo "\t{$option}\t{$description}\n";
 		}
 
-		$this->migrate_obj->up();
-		$this->init_set = true;
+		echo "\nDocumentation can be found at http://github.com/jnav24\n";
 	}
 
-	public function helpMigration()
+	public function buildMigrateArray($args)
 	{
-		echo "Here are some options \n";
-		print_r(options());
-	}
+		$delimiter = '-';
+		$run = [];
+		unset($args[0]);
 
-	public function executeMigration()
-	{
-		$this->migrate_obj->executeBaseMigration();
-		$this->init_set = false;
+		for ($i = 1; $i <= count($args); $i++) {
+			if (strpos($args[$i], $delimiter) > -1) {
+				$value = '';
+
+				if (isset($args[($i+1)]) && strpos($args[($i+1)], $delimiter) === false) {
+					$value = $args[($i+1)];
+				}
+
+				if (!array_key_exists($args[$i], $this->commands)) {
+					echo "The value `" . $args[$i] . "` does not exist. \n";
+					echo "\tRun `migrate -h`, if you want to know the list of commands.\n";
+					die();
+				}
+
+				$run[$args[$i]] = $value;
+			}
+		}
+
+		return $run;
 	}
 }
